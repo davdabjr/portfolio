4c5fd41aae28b7013a48ed1d9cc1dd81396a5539
davdabjr <davidcs1803@gmail.com>
2025-11-04 21:02:09 +0100

    Update Testimonials.tsx

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -14,7 +14,7 @@ export const Testimonials = () => {
           description="All the details about my achievements in every job"
         />
         <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
-          <div className="flex flex-none gap-8 pr-8 animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+          <div className="flex flex-none gap-8 pr-8 animate-move-left [animation-duration:40s] hover:[animation-play-state:paused]">
             {[...new Array(2)].fill(0).map((_, index) => (
               <Fragment key={index}>
                 {testimonials.map((testimonial, index) => (
@@ -22,20 +22,9 @@ export const Testimonials = () => {
                     key={index}
                     className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300"
                   >
-                    <div className="flex gap-4 items-center">
-                      <div className="size-14 bg-gray-700 inline-flex items-center rounded-full flex-shrink-0">
-                        <Image
-                          src={testimonial.avatar}
-                          alt={testimonial.name}
-                          className="max-h-full"
-                        />
-                      </div>
-                      <div>
+                    <div className="flex flex-col">
                         <div className="font-semibold">{testimonial.name}</div>
-                        <div className="text-sm text-white/40">
-                          {testimonial.position}
-                        </div>
-                      </div>
+                        <div className="text-sm text-white/40">{testimonial.position}</div>
                     </div>
                     <div className="mt-4 md:mt-6 text-sm md:text-base">
                       {testimonial.text}
