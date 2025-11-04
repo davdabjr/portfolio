9b045ef3f4eb3beb4b7f1649373559114c28690c
davdabjr <davidcs1803@gmail.com>
2025-11-04 21:15:52 +0100

    Update Testimonials.tsx

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,6 +1,5 @@
 import { SectionHeader } from "@/components/SectionHeader";
 import { testimonials } from "@/data/data";
-import Image from "next/image";
 import { Card } from "@/components/Card";
 import { Fragment } from "react";
 
@@ -9,36 +8,24 @@ export const Testimonials = () => {
     <section className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
-          eyebrow="Happy Clients"
-          title="What Clients Say about Me"
-          description="Don't just take my word for it. See what my clients have to say
-        about my work."
           eyebrow="More Info About My Jobs"
-          title="What I did in My Jobs"
-          description="All the details about my achievements in every job"
+          title="What I Did in My Jobs"
+          description="All the details about my achievements in every job."
         />
+
         <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
           <div className="flex flex-none gap-8 pr-8 animate-move-left [animation-duration:40s] hover:[animation-play-state:paused]">
             {[...new Array(2)].fill(0).map((_, index) => (
               <Fragment key={index}>
-                {testimonials.map((testimonial, index) => (
+                {testimonials.map((testimonial, i) => (
                   <Card
-                    key={index}
+                    key={i}
                     className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300"
                   >
-                    <div className="flex gap-4 items-center">
-                      <div className="size-14 bg-gray-700 inline-flex items-center rounded-full flex-shrink-0">
-                        <Image
-                          src={testimonial.avatar}
-                          alt={testimonial.name}
-                          className="max-h-full"
-                        />
-                      </div>
-                      <div>
-                        <div className="font-semibold">{testimonial.name}</div>
-                        <div className="text-sm text-white/40">
-                          {testimonial.position}
-                        </div>
+                    <div className="flex flex-col">
+                      <div className="font-semibold">{testimonial.name}</div>
+                      <div className="text-sm text-white/40">
+                        {testimonial.position}
                       </div>
                     </div>
                     <div className="mt-4 md:mt-6 text-sm md:text-base">
@@ -54,3 +41,4 @@ export const Testimonials = () => {
     </section>
   );
 };
+
