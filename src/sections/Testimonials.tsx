3baa6396f58917f9ca0235f2af2d6681dec17656
davdabjr <davidcs1803@gmail.com>
2025-11-04 21:12:13 +0100

    Update Testimonials.tsx

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -9,6 +9,10 @@ export const Testimonials = () => {
     <section className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
+          eyebrow="Happy Clients"
+          title="What Clients Say about Me"
+          description="Don't just take my word for it. See what my clients have to say
+        about my work."
           eyebrow="More Info About My Jobs"
           title="What I did in My Jobs"
           description="All the details about my achievements in every job"
@@ -22,9 +26,20 @@ export const Testimonials = () => {
                     key={index}
                     className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300"
                   >
-                    <div className="flex flex-col">
+                    <div className="flex gap-4 items-center">
+                      <div className="size-14 bg-gray-700 inline-flex items-center rounded-full flex-shrink-0">
+                        <Image
+                          src={testimonial.avatar}
+                          alt={testimonial.name}
+                          className="max-h-full"
+                        />
+                      </div>
+                      <div>
                         <div className="font-semibold">{testimonial.name}</div>
-                        <div className="text-sm text-white/40">{testimonial.position}</div>
+                        <div className="text-sm text-white/40">
+                          {testimonial.position}
+                        </div>
+                      </div>
                     </div>
                     <div className="mt-4 md:mt-6 text-sm md:text-base">
                       {testimonial.text}
